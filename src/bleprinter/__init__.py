d874f22a19ad5022f7e53b8f51fce1bb8ee339e2
FIGBERT <figbert@figbert.com>
2025-08-03 19:29:14 -0400

    Add dependencies and tweak project structure

--- src/bleprinter/__init__.py
+++ src/bleprinter/__init__.py
@@ -1,50 +1,10 @@
 import cv2
 import numpy as np
 
-from cat.ble import run_ble
-from cat.cmds import PRINT_WIDTH, cmds_print_img
-
-FONT_SIZE_MULTIPLIER = 0.5
-FONT_FAMILY = cv2.FONT_HERSHEY_DUPLEX
-
-
-class Text:
-    size: int
-    bold: bool
-    underline: bool
-    centered: bool
-    content: str
-
-    def __init__(
-        self,
-        content: str,
-        size: int = 1,
-        bold: bool = False,
-        underline: bool = False,
-        centered: bool = False,
-    ):
-        self.content = content
-        self.size = size
-        self.bold = bold
-        self.underline = underline
-        self.centered = centered
-
-    def dimensions(self) -> tuple[int, int]:
-        (width, height), baseline = cv2.getTextSize(
-            self.content,
-            FONT_FAMILY,
-            FONT_SIZE_MULTIPLIER * self.size,
-            self.thickness(),
-        )
-        return width, height + baseline
-
-    def thickness(self) -> int:
-        if self.bold:
-            if self.size > 3:
-                return 3
-            else:
-                return 2
-        return 1
+from bleprinter.ble import run_ble
+from bleprinter.cmds import PRINT_WIDTH, cmds_print_img
+from bleprinter.const import FONT_SIZE_MULTIPLIER, FONT_FAMILY
+from bleprinter.text import Text
 
 
 class CatPrinter:
